aec337a39baa307094e928ccc163c7e6fffc9a08
Blackscrche <blackscorche@gmail.com>
2025-04-01 01:41:29 +0100

    Changed all a tags to Link tag from react router dom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import {
   Facebook,
   Twitter,
@@ -26,11 +27,11 @@ function Footer() {
         <div>
           <h4 className="font-bold text-lg mb-4">Quick Links</h4>
           <ul>
-            <li><a href="/" className="hover:underline">Home</a></li>
-            <li><a href="/products" className="hover:underline">Products</a></li>
-            <li><a href="/pricelist" className="hover:underline">Pricelist</a></li>
-            <li><a href="/contact" className="hover:underline">Contact Us</a></li>
-            <li><a href="#" className="hover:underline">FAQs</a></li>
+            <li><Link to="/" className="hover:underline">Home</Link></li>
+            <li><Link to="/products" className="hover:underline">Products</Link></li>
+            <li><Link to="/pricelist" className="hover:underline">Pricelist</Link></li>
+            <li><Link to="/contact" className="hover:underline">Contact Us</Link></li>
+            <li><Link to="/faqs" className="hover:underline">FAQs</Link></li>
           </ul>
         </div>
 
@@ -44,15 +45,15 @@ function Footer() {
             </li>
             <li className="flex items-center space-x-3">
               <Phone className="text-blue-500 w-5 h-5" />
-              <span>+2348151162929</span>
+              <a href="tel:+2348151162929" className="hover:underline">+2348151162929</a>
             </li>
             <li className="flex items-center space-x-3">
               <Mail className="text-blue-500 w-5 h-5" />
-              <span>aztbstores@yahoo.com</span>
+              <a href="mailto:aztbstores@yahoo.com" className="hover:underline">aztbstores@yahoo.com</a>
             </li>
             <li className="flex items-center space-x-3">
               <MessageCircle className="text-blue-500 w-5 h-5" />
-              <span>WhatsApp Us</span>
+              <a href="https://wa.me/+2348151162929" target="_blank" rel="noopener noreferrer" className="hover:underline">WhatsApp Us</a>
             </li>
           </ul>
         </div>
@@ -62,22 +63,22 @@ function Footer() {
       <div className="text-center mt-8">
         <h4 className="font-bold text-lg mb-4">Follow Us</h4>
         <div className="flex justify-center space-x-4">
-          <a href="https://facebook.com/aztbstores" className="hover:text-blue-500">
+          <a href="https://facebook.com/aztbstores" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
             <Facebook className="w-6 h-6" />
           </a>
-          <a href="https://twitter.com/aztbstores" className="hover:text-blue-400">
+          <a href="https://twitter.com/aztbstores" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400">
             <Twitter className="w-6 h-6" />
           </a>
-          <a href="https://instagram.com/aztbstores" className="hover:text-pink-500">
+          <a href="https://instagram.com/aztbstores" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500">
             <Instagram className="w-6 h-6" />
           </a>
-          <a href="https://tiktok.com/@aztbstores" className="hover:text-black">
+          <a href="https://tiktok.com/@aztbstores" target="_blank" rel="noopener noreferrer" className="hover:text-black">
             <Music2 className="w-6 h-6" />
           </a>
-          <a href="https://snapchat.com/add/aztbstores" className="hover:text-yellow-500">
+          <a href="https://snapchat.com/add/aztbstores" target="_blank" rel="noopener noreferrer" className="hover:text-yellow-500">
             <Ghost className="w-6 h-6" />
           </a>
-          <a href="https://t.me/aztbstores" className="hover:text-blue-500">
+          <a href="https://t.me/aztbstores" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
             <Send className="w-6 h-6" />
           </a>
         </div>
